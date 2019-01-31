0042c56f3de83e5d8aecf72b867b5b522d2db4f4
John Burbridge <johnburbridge@gmail.com>
2019-01-31 13:23:16 -0800

    Simple session storage on redis

---
+++ frontend/src/main/kotlin/org/burbridge/spring/frontend/config/SessionConfig.kt
@@ -0,0 +1,19 @@
+package org.burbridge.spring.frontend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer
+
+@Profile("!test")
+@Configuration
+@EnableRedisHttpSession
+class SessionConfig : AbstractHttpSessionApplicationInitializer() {
+
+    @Bean
+    fun connectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory()
+    }
+}
